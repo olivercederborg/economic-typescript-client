ac26dafd4dfdd62eb9f62931fac47ad7585d0a34
Johan <johanfrolich@gmail.com>
2024-07-04 15:22:24 +0200

    Change generation script to support multiple API packages

--- bin/generate.sh
+++ bin/generate.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+
+input=$1
+name="$(echo $input | tr '[:upper:]' '[:lower:]')" # Lowercase
+file="$(tr '[:lower:]' '[:upper:]' <<< ${input:0:1})${input:1}" # Capitalize first letter
+echo "Generating $name and $file"
+
 openapi-generator generate \
   --generator-name typescript-axios \
-  --input-spec ./vendor/economic/openapi.json \
-  --output . \
+  --input-spec ./vendor/economic/openapi-$file.json \
+  --output ./packages/$name \
   --package-name=economic \
+  --skip-validate-spec \
   --additional-properties="withNodeImports=true" \
   --additional-properties="withSeparateModelsAndApi=true" \
-  --additional-properties="npmName=economic-client" \
+  --additional-properties="npmName=economic-client-$name" \
   --additional-properties="apiPackage=api" \
   --additional-properties="modelPackage=model" \
   --additional-properties="useSingleRequestParameter=true" \
